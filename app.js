692e74e2d62d2426280548976601864cf6794e05
jpreslov <lovett_jp@outlook.com>
2021-03-11 09:24:19 -0600

    reupload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const animals = ['dog', 'sea otter', 'mammoth', 'tricerotops']
+
+// for (starting point // how long loop should run // incrementation)
+for (let i = 0; i < animals.length; i++){
+    console.log("I like " + animals[i] + "s")
+}
